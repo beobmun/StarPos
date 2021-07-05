81c4c09e2f69f7890c410d7b9443fd835ee45419
beobmun <gksqjqans1@naver.com>
2021-07-05 23:48:05 +0900

    1.0

--- StarPos/SignUpViewController.swift
+++ StarPos/SignUpViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignUpViewController.swift
 //  StarPos
 //
 //  Created by 한법문 on 2021/07/05.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class SignUpViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
